f1225d4572c61888fed7c428944327ac2ce803b6
jenhee <hj1552016868@163.com>
2022-05-03 01:47:18 +0800

    锦鲤红包-本地log版

--- jd_jinli_locallog.ts
+++ jd_jinli_locallog.ts
@@ -7,7 +7,7 @@
  */
 
 import axios from 'axios';
-import {logs} from './util/jinli_log';
+import {logs} from './utils/jinli_log';
 import {getRandomNumberByRange, o2s, randomString, requireConfig, wait} from "./TS_USER_AGENTS";
 
 let cookie: string = '', res: any = '', UserName: string, UA: string = ''
