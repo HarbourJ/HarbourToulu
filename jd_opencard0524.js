4243906d6f8582927290f1d3ad42ad483c0ae7d5
jenhee <hj1552016868@163.com>
2022-05-24 12:47:04 +0800

    add

--- jd_opencard0524.js
+++ jd_opencard0524.js
@@ -51,7 +51,7 @@ let activityCookie =''
   $.activityId = "dzcdee05240f344982abf6fd43ceb0"
   $.shareUuid = "0f7fe453bf9e4df0b44c7c00b4032165"
   console.log(`入口:\nhttps://lzdz1-isv.isvjcloud.com/dingzhi/bigname/memberday/activity?activityId=${$.activityId}&shareUuid=${$.shareUuid}`)
-  let shareUuidArr = ["0f7fe453bf9e4df0b44c7c00b4032165"]
+  let shareUuidArr = ["0f7fe453bf9e4df0b44c7c00b4032165","378cc283fea5466585ceb2afb0d2d829"]
   let s = Math.floor((Math.random()*3))
   let n = 0
   n = Math.floor((Math.random()*shareUuidArr.length))
