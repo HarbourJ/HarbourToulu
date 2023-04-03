cb4671d5c68caf7802a82c3a984d573fa5fd7cc8
HarbourJ <37438113+HarbourJ@users.noreply.github.com>
2023-04-03 14:29:18 +0800

    update

--- jd_poolCaptainAJ.py
+++ jd_poolCaptainAJ.py
@@ -38,7 +38,7 @@ redis_port = os.environ.get("redis_port") if os.environ.get("redis_port") else "
 redis_pwd = os.environ.get("redis_pwd") if os.environ.get("redis_pwd") else ""
 jd_poolCaptainNums = os.environ.get("jd_poolCaptainNums") if os.environ.get("jd_poolCaptainNums") else "80"
 
-activityId = "36cc0f18d3eb4e178f2a3632f7af1c14"
+activityId = "8481334587da4707bbd553c413db28d9"
 shopId = "1000014486"
 print(f"【🛳活动入口】https://lzkjdz-isv.isvjcloud.com/pool/captain/8451632?activityId={activityId}")
 
